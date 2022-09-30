f3357dc8fcf7b02cb8ef2389673ece28a9e19ad2
timusss0 <mustikavia86@gmail.com>
2022-09-30 14:58:00 +0700

    pertama

---
+++ src/perulangan1/percobaan.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perulangan1;
+
+/**
+ *
+ * @author musti
+ */
+public class percobaan {
+   public static void main (String[] argumen){
+        for(int i=0; i <= 10; i++){
+            System.out.println(i + " ");
+        }
+    }
+}
+
