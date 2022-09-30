953cdef33bd982dcc539db9294d9555d051a2653
timusss0 <mustikavia86@gmail.com>
2022-09-30 20:44:05 +0700

    nambahin perhitungan luas belah ketupat

---
+++ src/perulangan4/perulangan4.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package perulangan4;
+
+/**
+ *
+ * @author musti
+ */
+public class perulangan4 {
+   public static void main (String[] args){
+       double diagonal1, diagonal2,luas;
+       diagonal1 = 12;
+       diagonal2 = 40;
+       luas = 0.5 * diagonal1 * diagonal2;
+       System.out.println(luas);
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+   }
+}
